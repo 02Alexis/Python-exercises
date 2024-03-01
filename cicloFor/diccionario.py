72337d662de078159ee8c9aefbd276cbd57e3674
02Alexis <m_chael1012@hotmail.com>
2024-03-01 13:49:47 -0500

    bucle for para iterar sobre los elementos del diccionario e imprime cada ítem

---
+++ cicloFor/diccionario.py
@@ -0,0 +1,6 @@
+# Se crea un diccionario con claves
+d = {'Banano':'$500', 'Mandarina':'$1000', 'Mango':'1000', }
+
+# Bucle for para iterar sobre los ítems del diccionario:
+for item in d.items():
+    print(item)
