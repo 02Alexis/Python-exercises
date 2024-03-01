9d82a75b0e2548a3f595670c94765a8d044cd11f
02Alexis <m_chael1012@hotmail.com>
2024-03-01 14:02:40 -0500

    se utiliza bucle while para imprimir el valor actual de x incrementando + 1, cuando alcanza o supera 5, el bucle se detiene

---
+++ ciclosWhile/cicloWhile.py
@@ -0,0 +1,10 @@
+# Se inicializa la variable x con el valor 0
+x = 0
+
+# Bucle while y aumento de x
+while x < 5:
+    print(f'el valor actual de x es {x}')
+    # En cada iteración, se imprime el valor actual de x y se incrementa en 1
+    x += 1
+else:
+    print('x no es mayor a')
