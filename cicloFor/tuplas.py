d819d384433f5c03f223b2eecc7408ccf4f3696d
02Alexis <m_chael1012@hotmail.com>
2024-03-01 13:45:39 -0500

    no habia guardado los cambios

---
+++ cicloFor/tuplas.py
@@ -0,0 +1,14 @@
+# creacion de la lista que contiene tuplas de dos elementos.
+mylist = [(1,2),(2,3),(3,4),(4,5),(5,6),(6,7),]
+
+# Imprime la longitud de la lista
+print(len(mylist))
+
+# Imprime cada elemento de la lista
+for item in mylist:
+    print(item)
+
+# Desempaqueta e imprime cada elemento de la lista por separado
+for (a,b) in mylist:
+    print(a)
+    print(a)
