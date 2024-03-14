2e6f17a88a984aaea5fe26d5ad5ac146a39f4572
02Alexis <m_chael1012@hotmail.com>
2024-03-14 13:15:30 -0500

    se crea una lista utilizando comprensión de listas, contiene los cuadrados de los números del 0 al 10

---
+++ comprension/square_list.py
@@ -0,0 +1,5 @@
+# Se utiliza la comprensión de listas para iterar sobre los números del 0 al 10 (exclusivamente), 
+# utilizando range(0, 11).
+# Para cada número x en ese rango, se eleva x al cuadrado usando x**2.
+mylist = [x**2 for x in range(0,11)]
+print(mylist) #crear una lista
