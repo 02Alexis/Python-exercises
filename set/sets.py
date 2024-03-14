50d313a0810c474ce0d9d278f660003fc989e841
02Alexis <m_chael1012@hotmail.com>
2024-03-14 15:43:10 -0500

    creacion de un conjunto vacío llamado y luego agregarle elementos al conjunto

---
+++ set/sets.py
@@ -0,0 +1,9 @@
+# Se crea un conjunto vacío llamado miset utilizando la función 'set()'
+miset = set()
+
+miset.add(1) # Se agrega el elemento 1 al conjunto utilizando
+miset.add(2) # Se agrega el elemento 2 al conjunto utilizando
+miset.add(3) # Se agrega el elemento 3 al conjunto utilizando
+
+# Se imprime el conjunto completo utilizando:
+print(miset)
