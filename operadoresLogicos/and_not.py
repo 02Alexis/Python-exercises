bd34314b3aef7620f61b69578d37d284ba415090
02Alexis <m_chael1012@hotmail.com>
2024-03-14 15:47:23 -0500

    usando operaodres logicos and y not

---
+++ operadoresLogicos/and_not.py
@@ -0,0 +1,9 @@
+hambre = True
+sed = True
+
+if hambre and not sed:
+    print('Tengo Hambre')
+elif hambre == True and sed == True:
+    print('Tengo Hambre y sed')
+else:
+    print('creo poder aguantar un poco mas')
