3f0d1c126fb9712429ec7ede82c3f29b721cb6b0
02Alexis <m_chael1012@hotmail.com>
2024-03-15 13:51:36 -0500

    usando metodo constructor vacio por defecto para dar valores a los atributos

--- POO/Abstraccion.py
+++ POO/Abstraccion.py
@@ -1,7 +1,15 @@
 class Personaje: #--------- creamos nombre de la clase
-    pass  #--------- indicamos que no haga nada de momento
+    #--------- darle personalidad con los atributos
+    #--------- damos una variable para cada atributo y le asignamos un valor por default
+    nombre = "alexis"
+    fuerza = 0
+    inteligencia = 0
+    defensa = 0
+    vida = 0
 
 #---------para crear un objeto con la plantilla utilizamos el metodo constructor de la clase
 #--------- es decir, Personaje(argumentos), de momento ninguno, lo guardamos en una var, 'mi_personaje'
 mi_personaje = Personaje()
-print(mi_personaje)
+mi_personaje.fuerza = 20 #--------- sobreescribimos los datos
+print("el nombre del jugador es ", mi_personaje.nombre) #---------imprimimos los valores
+print("el fuerza del jugador es", mi_personaje.fuerza)
