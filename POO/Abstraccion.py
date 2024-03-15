c48d9172027845863120a271668321265ac7e792
02Alexis <m_chael1012@hotmail.com>
2024-03-15 13:44:36 -0500

    iniciando con los pilares de poo, abstraccion, creacion de un personaje

---
+++ POO/Abstraccion.py
@@ -0,0 +1,7 @@
+class Personaje: #--------- creamos nombre de la clase
+    pass  #--------- indicamos que no haga nada de momento
+
+#---------para crear un objeto con la plantilla utilizamos el metodo constructor de la clase
+#--------- es decir, Personaje(argumentos), de momento ninguno, lo guardamos en una var, 'mi_personaje'
+mi_personaje = Personaje()
+print(mi_personaje)
