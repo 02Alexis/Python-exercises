1c14add5faff93af76970a176f3a5f8ff708ee71
02Alexis <m_chael1012@hotmail.com>
2024-03-15 13:59:43 -0500

    uso del metodo constructor def y declarando con self que atributos del objeto inicializaremos

--- POO/Abstraccion.py
+++ POO/Abstraccion.py
@@ -1,15 +1,16 @@
 class Personaje: #--------- creamos nombre de la clase
-    #--------- darle personalidad con los atributos
-    #--------- damos una variable para cada atributo y le asignamos un valor por default
-    nombre = "alexis"
-    fuerza = 0
-    inteligencia = 0
-    defensa = 0
-    vida = 0
+
+    #--------- 'self' argumento que hace referencia asi mismo al obj y permite atrabes del '.'
+    #--------- tener acceso a los atributos y metodos de la clase
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+        self.nombre = nombre
+        self.fuerza = fuerza
+        self.inteligencia = inteligencia
+        self.defensa = defensa
+        self.vida = vida
 
 #---------para crear un objeto con la plantilla utilizamos el metodo constructor de la clase
-#--------- es decir, Personaje(argumentos), de momento ninguno, lo guardamos en una var, 'mi_personaje'
-mi_personaje = Personaje()
-mi_personaje.fuerza = 20 #--------- sobreescribimos los datos
+#--------- es decir, Personaje(argumentos), le damos olos atributos y  lo guardamos en una var, 'mi_personaje'
+mi_personaje = Personaje("alex", 10, 3, 2, 50)
 print("el nombre del jugador es ", mi_personaje.nombre) #---------imprimimos los valores
 print("el fuerza del jugador es", mi_personaje.fuerza)
